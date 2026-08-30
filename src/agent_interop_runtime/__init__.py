3cfde64be517e70b6362487cc60dfc60567b789f
AHMED QAID <ahmed0qaid@gmail.com>
2026-08-30 17:52:58 +0300

    Export v0.3 handoff session and MCP APIs

--- src/agent_interop_runtime/__init__.py
+++ src/agent_interop_runtime/__init__.py
@@ -5,23 +5,42 @@ from .core import (
     MockAdapter,
     RunRequest,
     RunResult,
-    RuntimeAdapter,
     RuntimeEvent,
     RuntimePolicy,
 )
+from .handoff import (
+    AgentRoute,
+    ConversationTurn,
+    HandoffRequest,
+    HandoffResult,
+    HandoffRouter,
+    MemorySessionStore,
+    PortableHandoffEnvelope,
+    SessionState,
+)
 from .langgraph_adapter import LangGraphAdapter
+from .mcp_provider import MCPToolProvider, ToolDefinition
 from .openai_adapter import OpenAIAgentsAdapter
 
 __all__ = [
     "AdapterCapabilities",
+    "AgentRoute",
     "AgentSpec",
+    "ConversationTurn",
+    "HandoffRequest",
+    "HandoffResult",
+    "HandoffRouter",
     "InteropRuntime",
     "LangGraphAdapter",
+    "MCPToolProvider",
+    "MemorySessionStore",
     "MockAdapter",
     "OpenAIAgentsAdapter",
+    "PortableHandoffEnvelope",
     "RunRequest",
     "RunResult",
-    "RuntimeAdapter",
     "RuntimeEvent",
     "RuntimePolicy",
+    "SessionState",
+    "ToolDefinition",
 ]
